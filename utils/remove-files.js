fabd71e93f91c4458baeb224fa1e5fff64bf88c7
QA_Aleksey <aleksey.mikulin@abz.agency>
2021-12-23 13:06:28 +0200

    Fix problem with empty folders

--- utils/remove-files.js
+++ utils/remove-files.js
@@ -2,14 +2,14 @@ const path = require('path');
 const fs = require('fs');
 
 function removeFiles(pathToDirectory) {
-
     fs.readdir(pathToDirectory, (err, files) => {
         if (err) throw err;
 
         for (const file of files) {
-            fs.unlink(path.join(pathToDirectory, file), err => {
-                if (err) throw err;
-            });
+            let fullPathFile = path.join(pathToDirectory, file)
+            if (!(file === '.gitkeep')) {
+                fs.unlinkSync(fullPathFile)
+            }
         }
     });
 }
