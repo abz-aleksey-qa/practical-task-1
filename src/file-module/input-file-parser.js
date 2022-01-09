12c3f736427fb16371c795c748339aa25c50fd37
Татьяна Даньшина <admin@MacBook-Pro-Tatana.local>
2022-01-10 00:13:37 +0200

    Making changes according to paragraphs 3 - 5.e of the code review T1

---
+++ src/file-module/input-file-parser.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const parser = require('fast-csv');
+const path = require('path');
+const dataParser = require('../initial-data-parser');
+const outputFile = require('./output-file-creator')
+
+async function inputFilePareser(inputFilePath, ouptupFilePath) {
+    const extNameInputFile = path.extname(inputFilePath);
+
+    if (extNameInputFile == '.csv') {
+        const inputFile = await new Promise((resolve) => {
+            const results = [];
+            fs.createReadStream(inputFilePath)
+                .pipe(parser.parse({ headers: true }))
+                .on('error', error => { throw Error(error) })
+                .on('data', row => results.push(row))
+                .on('end', () => resolve(results));
+        });
+        let sortedInputFile = dataParser.sortFileData(inputFile);
+        dataParser.getOldestUser(sortedInputFile)
+        dataParser.getPopularLastName(sortedInputFile);
+        outputFile.createOuptuFileforInputData(sortedInputFile, extNameInputFile, ouptupFilePath)
+    }
+
+    if (extNameInputFile == '.json') {
+        const inputFile = JSON.parse(fs.readFileSync(inputFilePath, 'utf-8'));
+        let sortedInputFile = dataParser.sortFileData(inputFile);
+        dataParser.getOldestUser(sortedInputFile)
+        dataParser.getPopularLastName(sortedInputFile);
+        outputFile.createOuptuFileforInputData(sortedInputFile, extNameInputFile, ouptupFilePath)
+    }
+};
+
+module.exports = {
+    inputFilePareser
+};
